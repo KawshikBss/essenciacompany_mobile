c6a2845e1e341eb42dfb13f9865a8b847cc7053e
KawshikBss <kawshikbss@gmail.com>
2025-03-16 13:33:04 +0600

    refactor login view to use FormWidget for better structure and maintainability

--- lib/presentation/view/auth/login_view.dart
+++ lib/presentation/view/auth/login_view.dart
@@ -1,3 +1,4 @@
+import 'package:essenciacompany_mobile/presentation/component/form_widget.dart';
 import 'package:essenciacompany_mobile/presentation/component/layout/auth_layout.dart';
 import 'package:essenciacompany_mobile/presentation/component/text_input.dart';
 import 'package:flutter/material.dart';
@@ -15,56 +16,57 @@ class _LoginViewState extends State<LoginView> {
   @override
   Widget build(BuildContext context) {
     return AuthLayout(
-        child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        const Image(image: AssetImage('assets/logo.png')),
-        Container(
-          padding:
-              const EdgeInsets.only(top: 36, right: 40, left: 40, bottom: 54),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(30),
-            border: Border.all(color: Colors.black),
-          ),
-          child: Column(
-            children: [
-              TextInput(controller: _emailController, hintText: 'Email'),
-              const SizedBox(
-                height: 30,
-              ),
-              TextInput(
-                  controller: _passwordController,
-                  hintText: 'Password',
-                  obscureText: true),
-              const SizedBox(
-                height: 50,
-              ),
-              GestureDetector(
-                onTap: () {},
-                child: Container(
-                  padding: const EdgeInsets.symmetric(vertical: 12),
-                  decoration: BoxDecoration(
-                    color: const Color(0xFFF36A30),
-                    borderRadius: BorderRadius.circular(30),
-                  ),
-                  child: const Center(
-                    child: Text(
-                      'LOGIN',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 25,
-                        fontWeight: FontWeight.w400,
-                      ),
-                    ),
-                  ),
+        child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 28),
+            height: MediaQuery.of(context).size.height,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Image(
+                  image: AssetImage('assets/logo.png'),
+                  height: 120,
+                  fit: BoxFit.contain,
                 ),
-              )
-            ],
-          ),
-        )
-      ],
-    ));
+                const SizedBox(
+                  height: 50,
+                ),
+                FormWidget(
+                  children: [
+                    TextInput(controller: _emailController, hintText: 'Email'),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    TextInput(
+                        controller: _passwordController,
+                        hintText: 'Password',
+                        obscureText: true),
+                    const SizedBox(
+                      height: 50,
+                    ),
+                    GestureDetector(
+                      onTap: () {},
+                      child: Container(
+                        padding: const EdgeInsets.symmetric(vertical: 12),
+                        decoration: BoxDecoration(
+                          color: const Color(0xFFF36A30),
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        child: const Center(
+                          child: Text(
+                            'LOGIN',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 25,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                )
+              ],
+            )));
   }
 }
