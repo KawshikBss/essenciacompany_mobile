87b28acc97542b9c0146b02da4c340f84eac7fc3
KawshikBss <kawshikbss@gmail.com>
2025-03-18 12:03:03 +0600

    created food view

---
+++ lib/presentation/view/food_products/food_products_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FoodProductsView extends StatefulWidget {
+  const FoodProductsView({super.key});
+
+  @override
+  State<FoodProductsView> createState() => _FoodProductsViewState();
+}
+
+class _FoodProductsViewState extends State<FoodProductsView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Food Products View');
+  }
+}
