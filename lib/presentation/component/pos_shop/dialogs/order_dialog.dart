a315e9c8df4df55ee83a52733f8da348e92228c2
KawshikBss <kawshikbss@gmail.com>
2025-04-02 17:05:40 +0600

    add OrderDialog component for finalizing orders with user input fields

---
+++ lib/presentation/component/pos_shop/dialogs/order_dialog.dart
@@ -0,0 +1,201 @@
+import 'package:flutter/material.dart';
+
+class OrderDialog extends StatefulWidget {
+  const OrderDialog({super.key});
+
+  @override
+  State<OrderDialog> createState() => _OrderDialogState();
+}
+
+class _OrderDialogState extends State<OrderDialog> {
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: const Center(
+          child: Text(
+        'Finalize Order',
+        style: TextStyle(fontSize: 18),
+      )),
+      content: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Container(
+            padding: const EdgeInsets.all(6),
+            decoration: BoxDecoration(
+              color: const Color(0xff28badf),
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: const TextField(
+              textAlign: TextAlign.center,
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                hintText: 'Enter name',
+                hintStyle: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Container(
+            padding: const EdgeInsets.all(6),
+            decoration: BoxDecoration(
+              color: const Color(0xff28badf),
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: const TextField(
+              textAlign: TextAlign.center,
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                hintText: 'Enter email',
+                hintStyle: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 30),
+            decoration: BoxDecoration(
+              color: const Color(0xff28badf),
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                GestureDetector(
+                    onTap: () {},
+                    child: const Image(
+                      image: NetworkImage(
+                          'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/Flag_of_Portugal_%28alternate%29.svg/1200px-Flag_of_Portugal_%28alternate%29.svg.png'),
+                      width: 30,
+                      fit: BoxFit.fitWidth,
+                    )),
+                const Expanded(
+                    child: TextField(
+                  textAlign: TextAlign.center,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'Enter phone',
+                    hintStyle: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                    ),
+                  ),
+                ))
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Container(
+            padding: const EdgeInsets.all(6),
+            decoration: BoxDecoration(
+              color: const Color(0xff28badf),
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: const TextField(
+              textAlign: TextAlign.center,
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                hintText: 'Enter VAT number',
+                hintStyle: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Payment',
+                    style: TextStyle(
+                        fontSize: 18,
+                        color: Color(0xff28badf),
+                        fontWeight: FontWeight.w700),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 4, horizontal: 10),
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                                color: const Color(0xff28badf), width: 4)),
+                        child: const Text(
+                          'Card',
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Container(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 4, horizontal: 10),
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                                color: const Color(0xff28badf), width: 4)),
+                        child: const Text(
+                          'Withdraw',
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Invoice',
+                    style: TextStyle(
+                        fontSize: 18,
+                        color: Color(0xffec6031),
+                        fontWeight: FontWeight.w700),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                    padding:
+                        const EdgeInsets.symmetric(vertical: 4, horizontal: 10),
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                            color: const Color(0xffec6031), width: 4)),
+                    child: const Text(
+                      'Phone',
+                    ),
+                  )
+                ],
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
