4871256a04711a47b8c68f063e283f38d5c958db
KawshikBss <kawshikbss@gmail.com>
2025-05-08 16:45:25 +0600

    fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:essenciacompany_mobile/presentation/view/auth/login_view.dart';
-import 'package:essenciacompany_mobile/presentation/view/checkin_checkout/checkin_checkout_view.dart';
+// import 'package:essenciacompany_mobile/presentation/view/checkin_checkout/checkin_checkout_view.dart';
 import 'package:essenciacompany_mobile/presentation/view/enter_code_view.dart';
+import 'package:essenciacompany_mobile/presentation/view/my_wallet/wallet_view.dart';
 import 'package:essenciacompany_mobile/presentation/view/pos/orders_view.dart';
 import 'package:essenciacompany_mobile/presentation/view/pos/pos_shop_view.dart';
 import 'package:flutter/material.dart';
@@ -20,12 +21,13 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const CheckinCheckoutView(),
+      home: const LoginView(),
       routes: {
         '/login': (context) => const LoginView(),
         '/enter-code': (context) => const EnterCodeView(),
         '/pos/shop': (context) => const PosShopView(),
         '/pos/orders': (context) => const OrdersView(),
+        '/wallet': (context) => const WalletView(),
       },
     );
   }
