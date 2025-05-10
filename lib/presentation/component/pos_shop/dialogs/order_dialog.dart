3b0dfe5f36ce5ab9aa8cf44956a2a493641c0d24
KawshikBss <kawshikbss@gmail.com>
2025-05-11 00:29:37 +0600

    fixed search

--- lib/presentation/component/pos_shop/dialogs/order_dialog.dart
+++ lib/presentation/component/pos_shop/dialogs/order_dialog.dart
@@ -1,6 +1,7 @@
 import 'package:essenciacompany_mobile/core/cart_service.dart';
 import 'package:essenciacompany_mobile/domain/shop_requests.dart';
 import 'package:essenciacompany_mobile/presentation/component/pos_shop/dialogs/payment_confirm_dialog.dart';
+import 'package:essenciacompany_mobile/presentation/view/scanner_view.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -17,6 +18,8 @@ class _OrderDialogState extends State<OrderDialog> {
   String _invoice = 'Phone';
   String _paymentMethod = 'Card';
   CartService cartService = CartService();
+  String? _ticket;
+  Map<String, dynamic>? _walletUser;
 
   _toggleWithdraw() {
     setState(() {
@@ -62,12 +65,12 @@ class _OrderDialogState extends State<OrderDialog> {
     }).toList();
     if (_nameController.text.isEmpty ||
         _emailController.text.isEmpty ||
-        _phoneController.text.isEmpty ||
-        _vatController.text.isEmpty) {
+        _phoneController.text.isEmpty) {
       return;
     }
 
     Map<String, dynamic> orderData = {
+      'user_id': _walletUser != null ? _walletUser!['id'] : null,
       "extras": products,
       'billing': {
         'name': _nameController.text,
@@ -81,6 +84,7 @@ class _OrderDialogState extends State<OrderDialog> {
     };
     SharedPreferences prefs = await SharedPreferences.getInstance();
     String token = prefs.getString('token') ?? '';
+
     final res = await createOrder(orderData, token: token);
     if (res['success']) {
       _nameController.clear();
@@ -93,10 +97,44 @@ class _OrderDialogState extends State<OrderDialog> {
     }
   }
 
+  _makeQrPayment() async {
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return ScannerView(onScan: (data) {
+        setState(() {
+          _ticket = data;
+        });
+        Navigator.pop(context);
+      });
+    }));
+    final res = await getTicket(ticket: _ticket);
+    if (res['success']) {
+      final userData = res['data'];
+      setState(() {
+        _walletUser = userData;
+        _nameController.text = userData['name'] ?? _nameController.text;
+        _emailController.text = userData['email'] ?? _emailController.text;
+        _phoneController.text =
+            userData['contact_number'] ?? _phoneController.text;
+        _vatController.text = userData['vatNumber'] ?? _vatController.text;
+      });
+    }
+  }
+
+  _resetQr() {
+    setState(() {
+      _ticket = null;
+      _walletUser = null;
+      _nameController.text = '';
+      _emailController.text = '';
+      _phoneController.text = '';
+      _vatController.text = '';
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Dialog(
-      insetPadding: const EdgeInsets.all(20),
+      insetPadding: const EdgeInsets.all(12),
       child: Padding(
           padding: const EdgeInsets.all(20),
           child: Column(
@@ -110,6 +148,88 @@ class _OrderDialogState extends State<OrderDialog> {
               const SizedBox(
                 height: 20,
               ),
+              if (_paymentMethod == 'Wallet' && _walletUser != null)
+                GestureDetector(
+                  onTap: _resetQr,
+                  child: Container(
+                      padding: const EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8),
+                          color: Colors.white,
+                          border: Border.all(
+                              color: const Color(0xFFF2500B), width: 4)),
+                      child: const Align(
+                        alignment: Alignment.center,
+                        child: Text(
+                          'Reset',
+                          style:
+                              TextStyle(color: Color(0xFFF2500B), fontSize: 20),
+                          textAlign: TextAlign.center,
+                        ),
+                      )),
+                )
+              else if (_paymentMethod == 'Wallet')
+                GestureDetector(
+                  onTap: _makeQrPayment,
+                  child: Container(
+                      padding: const EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8),
+                          color: Colors.white,
+                          border: Border.all(
+                              color: const Color(0xFFF2500B), width: 4)),
+                      child: const Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(Icons.qr_code_scanner,
+                              size: 32, color: Color(0xFFF2500B)),
+                          SizedBox(
+                            width: 8,
+                          ),
+                          Text(
+                            'Pay with QR Code',
+                            style: TextStyle(
+                                color: Color(0xFFF2500B), fontSize: 20),
+                            textAlign: TextAlign.center,
+                          )
+                        ],
+                      )),
+                ),
+              if (_paymentMethod == 'Wallet' && _walletUser != null)
+                Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      if (_walletUser != null &&
+                          _walletUser!['name'] != null &&
+                          _walletUser!['name']!.isNotEmpty)
+                        Text(
+                          '${_walletUser!['name']}',
+                          style: const TextStyle(
+                              color: Color(0xFFF2500B), fontSize: 30),
+                          textAlign: TextAlign.start,
+                        ),
+                      if (_walletUser != null &&
+                          _walletUser!['email'] != null &&
+                          _walletUser!['email']!.isNotEmpty)
+                        Text(
+                          '${_walletUser!['email']}',
+                          style: const TextStyle(
+                              color: Color(0xFF676767), fontSize: 16),
+                          textAlign: TextAlign.start,
+                        ),
+                      if (_walletUser != null &&
+                          _walletUser!['balance'] != null)
+                        Text(
+                          'Є${_walletUser!['balance']}',
+                          style: const TextStyle(
+                              color: Color(0xFFF2500B), fontSize: 26),
+                          textAlign: TextAlign.start,
+                        ),
+                    ]),
+              const SizedBox(height: 20),
               Container(
                 padding: const EdgeInsets.all(6),
                 decoration: BoxDecoration(
@@ -250,10 +370,8 @@ class _OrderDialogState extends State<OrderDialog> {
                                   Icons.arrow_downward_sharp,
                                   size: 18,
                                 ),
-                                items: <String>[
-                                  'Card',
-                                  'Cash',
-                                ].map((String value) {
+                                items: <String>['Card', 'Cash', 'Wallet']
+                                    .map((String value) {
                                   return DropdownMenuItem<String>(
                                     value: value,
                                     child: Text(value),
